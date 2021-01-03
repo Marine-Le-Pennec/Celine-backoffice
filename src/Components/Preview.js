a3ca89c207ddd7d8cb052f6e7b22b4803e079731
OctoOcto <66621960+OctoOcto@users.noreply.github.com>
2021-01-03 17:14:04 +0100

    save

--- src/Components/Preview.js
+++ src/Components/Preview.js
@@ -86,23 +86,6 @@ const Preview = ({
 		</div>
 	));
 
-	// const currentThumbs = currentPictures.map((file, index) => (
-	// 	<div style={thumb} key={file.name}>
-	// 		<div style={thumbInner}>
-	// 			<img src={file.secure_url} style={img} alt='thumbnail' />
-	// 		</div>
-	// 		<button
-	// 			style={deleteButton}
-	// 			type='button'
-	// 			onClick={() => {
-	// 				const newCurrentPictures = [...currentPictures];
-	// 				newCurrentPictures.splice(index, 1);
-	// 				setCurrentPictures(newCurrentPictures);
-	// 			}}>
-	// 			<FontAwesomeIcon icon='times' color='yellow' size='lg' />
-	// 		</button>
-	// 	</div>
-	// ));
 	const currentThumbs = () => {
 		if (currentPictures) {
 			return currentPictures.map((file, index) => (
